c8f6117269269e166e54d477a83ff3a525250a70
Luis Rafael Sena <luisrafaelsen.20231@poli.ufrj.br>
2024-09-25 22:21:17 +0000

    Deletei WAVDataset.py e implementei a funcao get_data()

--- loader.py
+++ loader.py
@@ -2,8 +2,11 @@
 import os
 import datetime
 import typing
+import numpy as np
+from scipy.io import wavfile # Nao sei se tem alguma biblioteca no Lps_utils que recebe audios
 
 import lps_utils.utils as lps_utils
+from lps_sp.signal import decimate
 
 class DataLoader():
     ''' Dataloader para gerenciar os audios providos pelas boias '''
@@ -61,14 +64,44 @@ class DataLoader():
 
         return file_dict
 
-
     def get_data(self, buoy_id: int, start_time: datetime.datetime, end_time: datetime.datetime,
-                decimate=None):
+                decimate_num=None):
         '''
             Funcao para acessar e concatenar dados em um intervalo de tempo para determinada boia
         '''
 
+        data_resp = []
+        taxa = decimate_num
+        for time , audio_path in self.file_dict[buoy_id]:
+
+            # Posso fazer uma busca binaria pra achar o inicio e o fim na lista
+            # (se precisar otimizar depois , ai nao precisa iterar por todos os audios)
+
+            if start_time <= time <= end_time :
+
+                taxa_de_amostragem , dados = wavfile.read(audio_path)
+
+                if decimate_num :
+                    dados = decimate(dados, decimate_num)
+                elif not taxa :
+                    taxa = taxa_de_amostragem
+                elif taxa != taxa_de_amostragem :
+                    dados = decimate(dados, taxa_de_amostragem/taxa) # revisar isso aqui
+
+                data_resp.append(dados)
+
+        data_resp = np.concatenate(data_resp)
+
+        return taxa , data_resp
+
 
 if __name__ == "__main__":
-    DataLoader("./Data/RVT/raw_data")
+    data = DataLoader("./Data/RVT/raw_data")
+
+    start = datetime.datetime(2024, 1, 19, 13, 41, 0)
+    end = datetime.datetime(2024, 1, 19, 13, 45, 0)
+
+    taxa_ , data_ = data.get_data(2,start,end)
+
+    wavfile.write('teste.wav',taxa_,data_) # Nao consigo ouvir o audio, talvez eu tenha errado
     
