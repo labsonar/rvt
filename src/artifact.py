eeac08308410c7028d12f655760fe51e8af0c388
Luis Rafael Sena <luisrafaelsen.20231@poli.ufrj.br>
2024-10-03 23:55:03 +0000

    classe ArtifactManager corrigida e classe Artifact implementada.

--- src/artifact.py
+++ src/artifact.py
@@ -4,21 +4,44 @@ import datetime
 import bisect
 import pandas as pd
 
-class ArtifactLoader():
+class ArtifactManager():
     """ Class representing RVT Artifact system. """
 
-    def __init__(self, base_path="Artifacts.csv"):
+    def __init__(self, base_path="data/artifacts.csv"):
         self.path = base_path
         self.data = pd.read_csv(base_path)
+        self.index = 0
 
     def __len__(self) -> int:
-        """ Get the amount of diferent artifacts.
+        """ Get the amount of artifacts.
         
         Returns:
-            int: amount of diferent artifacts.
+            int: amount of artifacts.
+        """
+        return len(self.data)
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index < len(self.data):
+            value = Artifact(self.data["Artifact ID"][self.index])
+            self.index += 1
+            return value
+        raise StopIteration
+
+    def id_from_type(self, artifact_type: str) -> typing.List[int]:
+        """ Gets all artifacts id's of determined type
+
+        Args:
+            artifact_type (str): Type of the artifact.
+
+        Returns:
+            typing.List[int]: List with the id's of the artifacts whith same type
         """
 
-        return self.data["Tipo de Tiro"].nunique()
+        return self.data[self.data['Shooting Type'] == artifact_type]['Artifact ID'].tolist()
 
     def find_index(self, artifact_id: int) -> int:
         """ Find index from an artifact in a dataframe.
@@ -27,15 +50,15 @@ class ArtifactLoader():
             artifact_id (int): Artifact identification.
 
         Raises:
-            KeyError: Artifact id does not exist.
+            KeyError: Artifact ID does not exist.
 
         Returns:
             int: index in dataframe.
         """
 
-        index = bisect.bisect_left(self.data["ID artefato"] , artifact_id)
-        if self.data["ID artefato"][index] != artifact_id :
-            raise KeyError("Artifact id does not exist")
+        index = bisect.bisect_left(self.data["Artifact ID"] , artifact_id)
+        if self.data["Artifact ID"][index] != artifact_id :
+            raise KeyError("Artifact ID does not exist")
 
         return index
 
@@ -52,6 +75,8 @@ class ArtifactLoader():
             int: Amount of same type artifacts.
         """
 
+        # TODO Professor pediu pra mudar aq, mas eu n entendi # pylint: disable=fixme
+
         try:
             amount = self.data["Tipo de Tiro"].value_counts()[artifact_type]
         except KeyError as err:
@@ -59,52 +84,23 @@ class ArtifactLoader():
 
         return amount
 
-    def acess_data(self, artifact_id: int) -> typing.Dict[str, object]:
-        """ Guiven an artifact id, acess all data.
-
-        Args:
-            artifact_id (int): artifact identification.
-
-        Returns:
-            typing.Dict[str, object]: all data from determined artifact.
-        """
-
-        index = self.find_index(artifact_id)
-
-        data = {
-            "Index" : index ,
-            "ID artefato" : self.data.iloc[index]["ID artefato"] ,
-            "Tipo de Tiro" : self.data.iloc[index]["Tipo de Tiro"] ,
-            "Falha no Tiro" : bool(self.data.iloc[index]["Falha no Tiro"]) ,
-            "Caracterização" : self.data.iloc[index]["Caracterização"] ,
-            "Arquivo de Teste" : int(self.data.iloc[index]["Arquivo de Teste"]) ,
-            "Offsets" : self.get_time_all(artifact_id) ,
-            "Arquivos" : [self.data.iloc[index][f"Boia{i}-Arquivo"] for i in range(1, 6)]
-            }
-
-        return data
-
-    def get_time_all(self, artifact_id: int) -> typing.List[datetime.datetime]:
+    def get_time_all(self, artifact_id: int) -> typing.Dict[str, datetime.datetime]:
         """ Get the detection time of the artifact in all buoys.
 
             Args:
                 artifact_id (int): Identification of the artifact.
 
             Returns:
-                typing.List[datetime.datetime]: List with the detection time of each buoy.
+                typing.Dict[str, datetime.datetime]: Map with the detection time of each buoy.
         """
 
-        artifact_id = self.find_index(artifact_id)
-
-        # TODO quis indexar em 1 pra ficar de forma que mapa[boia_id] = offset de boia id, devo mudar?
-
-        mapa = [None]
+        mapa = {}
         for i in range(1,6):
 
             try :
-                mapa.append(self.get_time(artifact_id,i))
-            except KeyError:
-                mapa.append(None)
+                mapa[f"Buoy{i}"] = self.get_time(artifact_id,i)
+            except ValueError :
+                continue
 
         return mapa
 
@@ -117,27 +113,72 @@ class ArtifactLoader():
 
         Raises:
             KeyError: artifact_id or buoy_id does not exist.
+            ValueError: Buoy did not record the artifact.
 
         Returns:
             datetime.datetime: Time when the artifact was detected by the buoy.
-            None: Audio of artifact in buoy do not exist.
         """
-        # TODO Posso acessar diretamente o arquivo em que o artefato foi detectado # pylint: disable=fixme
-
-        artifact_id = self.find_index(artifact_id)
+        index = self.find_index(artifact_id)
 
         try :
-            str_date = str(self.data["Dia do Exercício"][artifact_id])
-            str_time = self.data[f"Boia{buoy_id}-offset"][artifact_id]
+            str_day = str(self.data["Exercise Day"][index])
+            str_file = str(self.data[f"Buoy{buoy_id}-File"][index])[2:-10]
+            str_offset = str(self.data[f"Buoy{buoy_id}-Offset"][index])
+
         except KeyError as out_of_range:
             raise KeyError("artifact_id or buoy_id does not exist") from out_of_range
 
         try :
-            time = datetime.datetime.strptime( str_date+str_time , "%Y%m%d%H:%M:%S.%f" )
-        except ValueError :
-            return None
+            # TODO Revisar isso aq, n sei se entendi a estrutura das datas na planilha # pylint: disable=fixme
+            date = datetime.datetime.strptime(str_day+str_file, "%Y%m%d_%H_%M")
+            offset_ = datetime.datetime.strptime(str_offset , "%H:%M:%S.%f")
+        except ValueError as err :
+            raise ValueError(f"Buoy{buoy_id} did not record the artifact {artifact_id}.") from err
 
-        return time
+        date = date + datetime.timedelta(
+                hours=offset_.hour,
+                minutes=offset_.minute,
+                seconds=offset_.second,
+                microseconds=offset_.microsecond
+            )
+
+        return date
+
+class Artifact(ArtifactManager):
+    """ Class representing an artifact. """
+
+    def __init__(self, artifact_id: int):
+        super().__init__()
+
+        # TODO esses atributos sao o suficiente ou eh bom ter mais informacao? # pylint: disable=fixme
+
+        self.iteration = 0
+        self.index = self.find_index(artifact_id)
+        self.id = artifact_id
+        self.type = self.data.iloc[self.index]["Shooting Type"]
+        self.failure = bool(self.data.iloc[self.index]["Shooting Failure"])
+        self.times = self.get_time_all(artifact_id)
+        self.keys = list(self.times.keys())
+
+    def __len__(self):
+        return len(self.times)
+
+    def __iter__(self):
+        self.iteration = 0
+        return self
+
+    def __next__(self):
+        if self.iteration < len(self.times):
+            key = self.keys[self.iteration]
+            value = self.times[key]
+            self.iteration += 1
+            return key , value
+        raise StopIteration
 
 if __name__ == "__main__":
-    store = ArtifactLoader()
+
+    artifact_manager = ArtifactManager()
+
+    for artifact in artifact_manager:
+        for buoy_id_, time in artifact:
+            print(f"[{buoy_id_}: {time}]")
