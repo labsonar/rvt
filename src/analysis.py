43f24ef75c9dd935d52ed07ea3d901d32ffc7da0
Gabriel Henrique Braga Lisboa <gabriel.lisboa@login-server.lps.ufrj.br>
2024-10-08 13:54:36 -0300

    WIP integracao com o id e o loader

--- src/analysis.py
+++ src/analysis.py
@@ -2,51 +2,55 @@ import wave
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
 
 # import lps_sp.signal as lps_signal
 import lps_sp.acoustical.analysis as lps_analysis
 import lps_sp.acoustical.broadband as lps_bb
+from artifact import ArtifactManager
+from loader import DataLoader
 
 class AudioAnalysis:
-    def __init__(self, audio_file, buoy_id, audio_id):
-        self.audio_file = audio_file
+    def __init__(self, audio, fs, duration, n_samples, buoy_id, audio_id):
+        self.audio_file = audio_id
         self.wave_file = None
-        self.signal = None
-        self.fs = None
+        self.audio = audio
+        self.fs = fs
+        self.duration = duration
+        self.n_samples = n_samples
         self.time_axis = None
         self.data_path = f'/home/gabriel.lisboa/Workspace/RVT/rvt/data/Analysis/Boia{buoy_id}/{audio_id}'
 
-    def get_audio(self, buoy_id):
-        '''Ainda tem que integrar com o loader e o identificador de artefatos'''
-        pass
+    # def load_audio(self):
+    #     """Carrega o arquivo .wav e armazena os dados do áudio"""
+    #     try:
+    #         self.wave_file = wave.open(self.audio_file, 'rb')
+    #         print(f"Arquivo de áudio {self.audio_file} carregado com sucesso.")
+    #         self.extract_signal()
+    #     except wave.Error as e:
+    #         print(f"Erro ao carregar o arquivo de áudio: {e}")
 
-    def load_audio(self):
-        """Carrega o arquivo .wav e armazena os dados do áudio"""
-        try:
-            self.wave_file = wave.open(self.audio_file, 'rb')
-            print(f"Arquivo de áudio {self.audio_file} carregado com sucesso.")
-            self.extract_signal()
-        except wave.Error as e:
-            print(f"Erro ao carregar o arquivo de áudio: {e}")
+    # def extract_signal(self):
+    #     """Extrai os dados do sinal do áudio"""
+    #     if self.wave_file:
+    #         self.fs = self.wave_file.getframerate()
+    #         num_frames = self.wave_file.getnframes()
 
-    def extract_signal(self):
-        """Extrai os dados do sinal do áudio"""
-        if self.wave_file:
-            self.fs = self.wave_file.getframerate()
-            num_frames = self.wave_file.getnframes()
+    #         audio_frames = self.wave_file.readframes(num_frames)
 
-            audio_frames = self.wave_file.readframes(num_frames)
+    #         self.audio = np.frombuffer(audio_frames, dtype=np.int16)
 
-            self.signal = np.frombuffer(audio_frames, dtype=np.int16)
-
-            duration = num_frames / float(self.fs)
-            self.time_axis = np.linspace(0, duration, num=num_frames)
+    #         duration = num_frames / float(self.fs)
+    #         self.time_axis = np.linspace(0, duration, num=num_frames)
 
     def plot(self, output_filename):
         """Salva o plot do áudio no tempo como um arquivo .png"""
-        if self.signal is not None and self.time_axis is not None:
+
+        self.time_axis = np.linspace(0, self.duration, num=self.n_samples)
+
+        if self.audio is not None and self.time_axis is not None:
             plt.figure(figsize=(10, 4))
-            plt.plot(self.time_axis, self.signal)
+            plt.plot(self.time_axis, self.audio)
             plt.title(f"Audio Waveform - {self.audio_file}")
             plt.xlabel("Time (seconds)")
             plt.ylabel("Amplitude")
@@ -60,7 +64,7 @@ class AudioAnalysis:
         
     def psd(self, output_filename):
         
-        psd_freq, psd_result = lps_bb.psd(signal=self.signal, fs=self.fs, window_size=4096, overlap=0.5)
+        psd_freq, psd_result = lps_bb.psd(signal=self.audio, fs=self.fs, window_size=4096, overlap=0.5)
 
         plt.figure(figsize=(12, 6))
         plt.plot(psd_freq, psd_result)
@@ -73,8 +77,8 @@ class AudioAnalysis:
         plt.close()
 
     def fft(self, output_filename):
-        fft_result = np.fft.fft(self.signal)
-        fft_freq = np.fft.fftfreq(len(self.signal), 1/self.fs)
+        fft_result = np.fft.fft(self.audio)
+        fft_freq = np.fft.fftfreq(len(self.audio), 1/self.fs)
 
         plt.figure()
         plt.plot(fft_freq[:len(fft_result)//2], 
@@ -90,7 +94,7 @@ class AudioAnalysis:
 
     def lofar(self, output_filename):
 
-        power, freq, time = lps_analysis.SpectralAnalysis.lofar(self.signal, self.fs)
+        power, freq, time = lps_analysis.SpectralAnalysis.lofar(self.audio, self.fs)
 
         plt.figure()
         plt.pcolormesh(time, freq, 10 * np.log10(power), shading='gouraud')
@@ -107,6 +111,36 @@ class AudioAnalysis:
         if self.wave_file:
             self.wave_file.close()
             print(f"Arquivo de áudio {self.audio_file} fechado.")
+
+def artifact_analysis():
+
+    artifact_manager = ArtifactManager(base_path="/home/gabriel.lisboa/Workspace/RVT/rvt/data/artifacts.csv")
+
+    for artifact in artifact_manager:
+        
+        for buoy_id, time in artifact:
+
+            start_time = time - timedelta(seconds=10)
+            end_time = time + timedelta(seconds=2)
+
+            duration = (end_time - start_time).total_seconds()
+            
+            # audio_analysis.duration = end_time - start_time
+
+            loader = DataLoader("/home/gabriel.lisboa/Workspace/RVT/Data/RVT/raw_data")
+            
+            fs, audio = loader.get_data(buoy_id, start_time, end_time)
+
+            n_samples = int(duration * fs)
+
+            audio_analysis = AudioAnalysis(audio, fs, duration, n_samples, buoy_id, time)
+
+            audio_analysis.plot(f'{time}.png')
+            audio_analysis.psd(f'{time}.png')
+            audio_analysis.fft(f'{time}.png')
+            audio_analysis.lofar(f'{time}.png')
+
+            
 
 if __name__ == "__main__":
     path_to_file = '/home/gabriel.lisboa/Workspace/RVT/Data/RVT/raw_data/20240119/boia1'
